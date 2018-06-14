e61641a4b20ee51032903343655da5fb55fd55aa
isolatedoatmeal <danielmilton@daniels-macbook-pro-3.local>
2018-06-14 10:23:46 -0700

    Initial Commit

---
+++ Travel Social media/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travel Social media
+//
+//  Created by Daniel Milton on 6/14/18.
+//  Copyright © 2018 IsolatedOatmeal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
